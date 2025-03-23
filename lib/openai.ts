6b061b6a33f372237f8d447a9f09d759a719debf
Hendar <hendarsundana@gmail.com>
2025-03-23 21:56:15 +0700

    page login and generator AI

--- lib/openai.ts
+++ lib/openai.ts
@@ -2,9 +2,8 @@ import { createOpenAI } from "@ai-sdk/openai"
 
 // Create a custom OpenAI provider instance with your API key
 export const openaiClient = createOpenAI({
-  //apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
   // You can add other options here
-  apiKey: "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"
   compatibility: "strict", // Use strict mode for the OpenAI API
 })
 
